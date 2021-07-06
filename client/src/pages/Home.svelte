94e47ba38ceb217dfa51764cf1ade23614bf341b
leofishman <leofishman@gmail.com>
2021-07-06 15:48:18 -0300

    welcome

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,7 +1,7 @@
 <section class="hero is-link is-fullheight-with-navbar">
   <div class="hero-body">
     <div class="container">
-      <h2 class="title">💰Expenses Tracking made easy</h2>
+      <h2 class="title">Registrese para comenzar a usar el sistema</h2>
       <a href="#/signup" class="button is-primary is-light">Get Started</a>
     </div>
   </div>
