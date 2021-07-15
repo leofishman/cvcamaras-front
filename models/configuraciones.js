9e5b4cd5343f356adbd305449fd3ebff6d894fc0
leofishman <leofishman@gmail.com>
2021-07-15 11:02:36 -0300

    remove types phone and email

--- models/configuraciones.js
+++ models/configuraciones.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const mongooseTypePhone = require('mongoose-type-phone')
-require('mongoose-type-email')
+//const mongooseTypePhone = require('mongoose-type-phone')
+//require('mongoose-type-email')
 
 const configuracionesSchema = new mongoose.Schema ({
-  alertas_email: { type: mongoose.SchemaTypes.Email, default: 'alertas@mail.com'},
+  alertas_email: mongoose.SchemaTypes.String,
   alertas_telefono: mongoose.SchemaTypes.Number, // TODO: Change to phone type
   alertas_periodicidad: mongoose.SchemaTypes.Number,
 })
